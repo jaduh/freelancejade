6fa1ab43473bf294d130dec74cca6f9de6579681
Jade Yorks <jadeyorks@MacBook-Pro-van-Jade.local>
2020-01-08 14:17:39 +0100

    lijstjes toegevoegd aan aboutpage

--- src/pages/about.js
+++ src/pages/about.js
@@ -27,13 +27,65 @@ const AboutPage = () => (
       <p>Mijn missie is om mensen en bedrijven die het beste voor hebben met de wereld te helpen om hun boodschap bij de juiste mensen over te brengen. Niet als vluchtig vergeten nieuwsfeiten, maar als aantrekkelijke verhalen waar mensen maar niet over uitgepraat raken.
 </p>
 <div className="center-column">
-<h2>Meer weten?</h2>
+<h3>Meer weten?</h3>
 <a href="https://expert-experimenter-9255.ck.page/209f4ab0d8
 "><button>Schrijf je in voor mijn nieuwsbrief</button></a>
-<a href="/"><h2>Of volg mijn blog</h2></a>
+<a href="/"><h4>Volg mijn blog</h4></a>
+
+<a href="mailto:jadeyorks@gmail.com?Subject=Hoi%20Ik%20moet%20je%20spreken!again" target="_top"><h4>Of praat met mij</h4></a>
+
+
+
+
+
 </div>
+<h2>Omdat ik van lijstjes houd (wie niet?), vind je hieronder een collectie van lijstjes over mij.</h2>
+<h4>Leven</h4>
+<ul>
+  <li>Geboren in Den Haag
+  </li>
+  <li>Uit bouwjaar ‘86
+  </li>
+  <li>Woont momenteel in Antwerpen, maar voelt zich soms nog steeds wat Haags en  Paramaribo’s
+  </li>
+  <li>Zette maar liefst twee keer een lief en temperament vol mensje op de wereld
+  </li>
+</ul>
+<h4>Werk</h4>
+<ul>
+  <li>Expert in verhalen maken
+  </li>
+  <li>Sterke storytelling en content marketingskills
+  </li>
+  <li>Gepassioneerd over het maken van verhalen die oplossingen aanreiken voor problemen
+  </li>
+  <li>Goede communicatie- en samenwerkingsskills
+  </li>
+  <li>Ervaring met doelgroepgerichte contentcreatie
+  </li>
+  <li>Uitmuntende luisterskills
+  </li>
+</ul>
+<h4>Waar ik in wil verbeteren</h4>
+<li>E-mailmarketing
+</li>
+<li>Sterkere video(nabewerkings)skills
+</li>
+<li>Programmeren van interactieve webapplicaties op maat van een goed verhaal
+</li>
     </div>
   </div>
 )
+
+
+
+
+
+
+
+
+
+
+
 
 export default AboutPage
